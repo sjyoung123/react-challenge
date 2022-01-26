80109c97e9c293b399786c6ac03988d443c3179b
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-01-26 17:02:26 +0900

    Set default environments

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const dark: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#44bd32",
+};
+
+export const light: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#2f3640",
+  accentColor: "#44bd32",
+};
